8bc4f0b77ce52f5ea0c9a3c253ada0ea2361d1c4
samgusa <samgusaapp1993@gmail.com>
2023-04-16 16:10:30 -0400

    Initial Commit

---
+++ VRailsTest/VRailsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VRailsTestApp.swift
+//  VRailsTest
+//
+//  Created by Sam Greenhill on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct VRailsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
